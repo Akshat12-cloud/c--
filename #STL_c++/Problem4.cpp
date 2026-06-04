b9937e2ca32a1ea20f298ec9779db4c6e4c947fc
Akshat12-cloud <akshatrk37@gmail.com>
2026-06-04 18:46:04 +0530

    upload

---
+++ #STL_c++/Problem4.cpp
@@ -0,0 +1,19 @@
+//#STACK--->
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<string> s;
+    s.push("love");
+    s.push("babbar");
+    s.push("kumar");
+    cout<<"top element :"<<s.top()<<endl;
+    s.pop();
+    cout<<"top element :"<<s.top()<<endl;
+    cout<<"size of the stack :"<<s.size()<<endl;
+    cout<<"empty or not :"<<s.empty()<<endl;
+    return 0;
+
+
+
+}
