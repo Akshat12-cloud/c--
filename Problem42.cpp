fd8ada8917e97b77aac26c8c2fba6598c981ca28
Akshat12-cloud <akshatrk37@gmail.com>
2026-06-08 19:32:38 +0530

    upload

---
+++ Problem42.cpp
@@ -0,0 +1,19 @@
+//find length of the string.
+#include<iostream>
+using namespace std;
+int getlength(char name[]){
+    int count=0;
+    for(int i=0;name[i]!=0;i++){
+        count++;
+    }
+    return count;
+}
+int main(){
+    char name[20];
+    cout<<"enter the name:"<<endl;
+    cin>>name;
+    cout<<"your name is :"<<name<<endl;
+    cout<<"length :"<<getlength(name);
+    return 0;
+
+}
