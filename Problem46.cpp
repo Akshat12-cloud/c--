c5b40e9917633b61312c1b7853872ff82e6cdc7f
Akshat12-cloud <akshatrk37@gmail.com>
2026-06-09 23:46:13 +0530

    upload

---
+++ Problem46.cpp
@@ -0,0 +1,24 @@
+//replace spaces.
+#include<iostream>
+#include<string>
+using namespace std;
+string replacespaces(string &s){
+    string temp="";
+    for (int i=0;i<s.length();i++){
+        if(s[i] == ' '){
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+
+        }
+        else{
+            temp.push_back(s[i]);
+        }
+    }
+    return temp;
+}
+int main(){
+    string s="my name is akshat";
+    cout<<replacespaces(s);
+    return 0;
+}
