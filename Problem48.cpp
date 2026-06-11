43712238cf9433db0c556b9384c951304edee120
Akshat12-cloud <akshatrk37@gmail.com>
2026-06-11 18:51:54 +0530

    upload

--- Problem48.cpp
+++ Problem48.cpp
@@ -1,1 +1,34 @@
 //get max occurance.
+#include <iostream>
+#include <string>
+using namespace std;
+
+char getMaxOccChar(string s) {
+    int count[26] = {0};
+
+    // Count frequency
+    for (int i = 0; i < s.length(); i++) {
+        int index = s[i] - 'a';
+        count[index]++;
+    }
+
+    // Find maximum frequency
+    int maxi = -1;
+    int ans = 0;
+
+    for (int i = 0; i < 26; i++) {
+        if (count[i] > maxi) {
+            maxi = count[i];
+            ans = i;
+        }
+    }
+
+    return 'a' + ans;
+}
+
+int main() {
+    string s = "testsample";
+    cout << "Maximum occurring character: "
+         << getMaxOccChar(s) << endl;
+    return 0;
+}
