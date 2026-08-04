6ec0956522f381078d580c8bd1580cb155063914
Akshat12-cloud <akshatrk37@gmail.com>
2026-08-05 01:11:43 +0530

    upload

--- Problem56.cpp
+++ Problem56.cpp
@@ -7,23 +7,25 @@ using namespace std;
 void rotate(vector<int>& nums, int k) {
     int n = nums.size();
 
-    while (k--) {
-        int last = nums[n - 1];
+    k = k % n;
 
-        for (int i = n - 1; i > 0; i--) {
-            nums[i] = nums[i - 1];
-        }
+    vector<int> temp(n);
 
-        nums[0] = last;
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = nums[i];
     }
+
+    nums = temp;
 }
 
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
 
-    rotate(nums, 2);
+    rotate(nums, 3);
 
-    for (int num : nums) {
-        cout << num << " ";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
     }
+
+    return 0;
 }
