68b7c4b0c1f1adef546a9985bf0c0e7cd3de8c0c
Akshat12-cloud <akshatrk37@gmail.com>
2026-08-06 20:24:37 +0530

    upload

--- Problem59.cpp
+++ Problem59.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
-int maxsub(int arr[],int n){
-    for(int i=0;i<n;i++){
-        
+void rowsum(int arr[][4],int row ,int col){
+    
+    for(int row=0;row<3;row++){
+        int sum=0;
+        for(int col=0;col<4;col++){
+            sum+=arr[row][col];
+        }
+        cout<<sum<<" ";
     }
+
+}
+int main(){
+    int arr[3][4]={{1,2,3,4},{2,4,6,8},{3,7,9,10}};
+    rowsum(arr,3,4);
+    return 0;
 }
