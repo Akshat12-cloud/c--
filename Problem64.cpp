ca5426bcf376bfda4f46d3c0bd4c88977a94a8ce
Akshat12-cloud <akshatrk37@gmail.com>
2026-08-10 23:14:51 +0530

    upload

--- Problem64.cpp
+++ Problem64.cpp
@@ -1,12 +1,24 @@
 //Move all zeroes to the end on an array.
 #include<iostream>
+#include<vector>
 using namespace std;
-int movezeroes(int arr[],int n){
-    
+vector<int> movezeroes(vector<int> &arr){
+    int n=arr.size();
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(arr[i] !=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    return arr;
 }
 int main(){
-    int arr[10]={12,3,4,5,0,5,0,6,0};
-    movezeroes(arr,10);
+    vector<int> arr={12,3,4,5,0,5,0,6,0};
+    movezeroes(arr);
+    for(int i=0; i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
 
