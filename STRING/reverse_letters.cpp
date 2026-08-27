f0338a492c557899e8ab0fdbbff74b47dfffebba
Akshat12-cloud <akshatrk37@gmail.com>
2026-08-27 23:57:23 +0530

    upload

---
+++ STRING/reverse_letters.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+    string ch="let's start string";
+    int length=ch.length();
+    int s=0;
+    int e=length;
+    while(s<=e){
+        swap(ch[s],ch[e]);      
+    }
+    int newstart;
+    int newend;
+    for(int i=0;i<ch.length();i++){
+        newnend++;
+        if(ch[i] != )
+    }
+}
