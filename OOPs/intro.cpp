04e073b693b2255b3a3ab2b2b4cab91ac12d6477
Akshat12-cloud <akshatrk37@gmail.com>
2026-08-31 23:35:20 +0530

    upload

---
+++ OOPs/intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class Hero {
+    //Properties
+    int health;
+};
+int main(){
+
+    //creation of object.
+    Hero h1;
+    cout<<"size :"<<sizeof(h1)<<endl;
+    return 0;
+}
