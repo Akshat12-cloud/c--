081a81d36855ffd7655f73bd6ab3dfbda15fbc20
Akshat12-cloud <akshatrk37@gmail.com>
2026-09-03 23:33:17 +0530

    upload

---
+++ OOPs/single_inheritance.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class animal{
+    public:
+    int age;
+    int weight;
+    public:
+    void speak(){
+        cout<<"speaking"<<endl;
+    }
+};
+class dog:public animal{
+
+
+};
+int main(){
+    dog d;
+    d.speak();
+    cout<<d.age<<endl;
+    cout<<d.weight<<endl;
+    return 0;
+}
